ff9d819d1caefd14ad89ed4c8cdd0ea68f06dcda
Fan <fanweng02@gmail.com>
2023-03-28 23:31:58 -0400

    Set up load balancer with nginx.

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker run --rm -itd -v $(pwd)/autocomplete_server:/autocomplete -p 8080:80 --name autocomplete goimg:0.1
-docker run --rm -itd -v $(pwd)/reverse_index_server/data:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name reverse_index esimg:0.1
+docker run --rm -itd -v $(pwd)/autocomplete_server:/autocomplete -p 8000:80 --name autocomplete goimg:0.1
+docker run --rm -itd -v $(pwd)/reverse_index_server/data:/usr/share/elasticsearch/data -p 8001:9200 -p 8002:9300 -e "discovery.type=single-node" --name reverse_index esimg:0.1
+docker run -itd -v $(pwd)/load_balancer/nginx.conf:/etc/nginx/nginx.conf:ro -p 8003:80 --name load_balancer nginximg:0.1
