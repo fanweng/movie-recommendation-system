27570031d3dd9c142975c9b8d283ce56b2daa03a
Fan <fanweng@users.noreply.github.com>
2023-04-01 12:19:05 -0400

    Start autocomplete program when server starts. (#4)

--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -itd -v $(pwd)/autocomplete_server:/autocomplete -p 8080:80 --name autocomplete goimg:0.1
+docker run --rm -itd -v $(pwd)/autocomplete_server:/autocomplete -p 8080:80 --name autocomplete goimg:0.2
 docker run --rm -itd -v $(pwd)/reverse_index_server/data:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" --name reverse_index esimg:0.1
